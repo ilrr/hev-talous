616821b602004d5b075f019e823c706e0c340228
Ilari <ilari.rauhala@gmail.com>
2024-03-07 19:38:37 +0200

    v0.1

---
+++ src/main.rs
@@ -0,0 +1,93 @@
+use std::{
+    env,
+    fs::{self, read_to_string},
+};
+
+use calamine::{open_workbook, Data,  Reader, Xlsx};
+use tlk::{Sexpr, A};
+
+use crate::tlk::parse;
+
+mod tlk;
+
+fn row_to_sexpr(row: &[Data], event_index: &mut i32) -> Sexpr {
+    let date = Sexpr::List(
+        row[0]
+            .to_string()
+            .split('.')
+            .rev()
+            .map(|v| Sexpr::Atom(A::Number(v.parse::<i32>().expect("Huono päivämäärä"))))
+            .collect::<Vec<Sexpr>>(),
+    );
+
+    let column_6 = row[6].to_string();
+    let description = (if column_6.len() > 0 {
+        format!(
+            "{} — {} – {}",
+            row[4].to_string(),
+            row[5].to_string(),
+            row[6].to_string()
+        )
+    } else {
+        format!("{} — {}", row[4].to_string(), row[5].to_string())
+    }).replace("\n", r#"\n"#);
+    let account = match row[3].to_string().as_str() {
+        "Palvelumaksut" => 3210,
+        s => s[..4].parse().expect("Excelissä on jotain häikkää..."),
+    };
+    let amount: i32 = (row[7]
+        .to_string()
+        .parse::<f32>()
+        .expect("Excelissä on jotain häikkää...")
+        * 100.0) as i32;
+    let i = event_index.to_owned();
+    *event_index += 1;
+    Sexpr::List(vec![
+        Sexpr::Atom(A::Symbol("event".to_string())),
+        Sexpr::Atom(A::Number(i)),
+        date,
+        Sexpr::Atom(A::String(description)),
+        Sexpr::List(vec![
+            Sexpr::List(vec![
+                Sexpr::Atom(A::Number(1130)),
+                Sexpr::List(vec![
+                    Sexpr::Atom(A::Symbol("money".to_string())),
+                    Sexpr::Atom(A::Number(amount)),
+                ]),
+            ]),
+            Sexpr::List(vec![
+                Sexpr::Atom(A::Number(account)),
+                Sexpr::List(vec![
+                    Sexpr::Atom(A::Symbol("money".to_string())),
+                    Sexpr::Atom(A::Number(-amount)),
+                ]),
+            ]),
+        ]),
+    ])
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let workbook = &args[1];
+    let input_ledger = &args[2];
+    let output_ledger = &args[3];
+
+    let mut w: Xlsx<_> =
+        open_workbook(workbook).expect(&format!("Virheellinen Excel-polku: {workbook}"));
+    let mut event_index: i32 = 0;
+    let mut events: Vec<Sexpr> = Vec::new();
+
+    if let Ok(range) = w.worksheet_range("Päiväkirja") {
+        range
+            .rows()
+            .skip(6)
+            .for_each(|r| events.push(row_to_sexpr(r, &mut event_index)));
+    }
+
+    let account = read_to_string(input_ledger).expect("Tiedoston luku ei onnistunut.");
+    let mut a = parse(account);
+
+    a.mutate_5_5(Sexpr::List(events));
+
+    fs::write(output_ledger, a.to_string()).expect("Tiedoston kirjoittaminen epäonnistui");
+}
---
+++ src/tlk.rs
@@ -0,0 +1,145 @@
+use std::{
+    fmt,
+    fs::write,
+    iter::{self, from_fn},
+};
+
+pub enum Sexpr {
+    Atom(A),
+    List(Vec<Sexpr>),
+}
+
+pub enum A {
+    String(String),
+    Number(i32),
+    Symbol(String),
+}
+
+impl fmt::Display for Sexpr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Sexpr::Atom(a) => write!(f, "{}", a.to_string()),
+            Sexpr::List(l) => write!(
+                f,
+                "({})",
+                l.iter()
+                    .map(|v| v.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            ),
+        }
+    }
+}
+
+impl Sexpr {
+    pub fn push(&mut self, v: Sexpr) {
+        match self {
+            Self::List(l) => l.push(v),
+            Self::Atom(_) => panic!("Can't push to atom!"),
+        }
+    }
+    pub fn get(&self, i: usize) -> &Sexpr {
+        match self {
+            Self::List(l) => l.get(i).unwrap(),
+            Self::Atom(_) => panic!("Cant index atom!"),
+        }
+    }
+    pub fn get_mut(&mut self, i: usize) -> &Sexpr {
+        match self {
+            Self::List(l) => l.get_mut(i).unwrap(),
+            Self::Atom(_) => panic!("Cant index atom!"),
+        }
+    }
+    pub fn mutate_5_5(&mut self, v: Sexpr) {
+        if let Sexpr::List(l1) = self {
+            if let Some(s1) = l1.get_mut(5) {
+                if let Sexpr::List(l2) = s1 {
+                    if let Some(s2) = l2.get_mut(5) {
+                        *s2 = v;
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl fmt::Display for A {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            A::String(s) => write!(f, "\"{}\"", s),
+            A::Number(n) => write!(f, "{}", n),
+            A::Symbol(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+trait Stack<T> {
+    fn new() -> Self;
+    fn push(&mut self, v: T) -> ();
+    fn pop(&mut self) -> T;
+    fn push_top(&mut self, v: T) -> ();
+    fn push_empty(&mut self) -> ();
+}
+
+impl Stack<Sexpr> for Vec<Sexpr> {
+    fn new() -> Self {
+        Vec::new()
+    }
+    fn push(&mut self, v: Sexpr) -> () {
+        self.push(v);
+    }
+    fn pop(&mut self) -> Sexpr {
+        self.pop().expect("Empty stack")
+    }
+    fn push_top(&mut self, v: Sexpr) -> () {
+        self.last_mut().expect("Error").push(v);
+    }
+    fn push_empty(&mut self) -> () {
+        self.push(Sexpr::List(Vec::new()));
+    }
+}
+
+pub fn parse(s: String) -> Sexpr {
+    let mut stack: Vec<Sexpr> = Vec::new();
+    let mut iter = s.chars().peekable();
+    while let Some(ch) = iter.next() {
+        match ch {
+            '(' => stack.push_empty(),
+            ')' => {
+                let top = stack.pop().expect("Unmatched parentheses");
+                // println!("1- {top}");
+                if stack.is_empty() {
+                    return top;
+                }
+                stack.push_top(top);
+            }
+            '"' => {
+                let s: String = iter::once(ch)
+                    .chain(from_fn(|| iter.by_ref().next_if(|c| *c != '"')))
+                    .collect();
+                iter.next();
+                // println!("{s}");
+                stack.push_top(Sexpr::Atom(A::String(s[1..].to_string())));
+            }
+            '0'..='9' | '-' => {
+                let n: i32 = iter::once(ch)
+                    .chain(from_fn(|| iter.by_ref().next_if(|c| c.is_ascii_digit())))
+                    .collect::<String>()
+                    .parse()
+                    .unwrap();
+                stack.push_top(Sexpr::Atom(A::Number(n)))
+            }
+            c if c.is_whitespace() => {}
+
+            _ => {
+                let s: String = iter::once(ch)
+                    .chain(from_fn(|| iter.by_ref().next_if(|c| *c != ' ')))
+                    .collect();
+                iter.next();
+                // println!("{s}");
+                stack.push_top(Sexpr::Atom(A::Symbol(s.to_string())));
+            }
+        }
+    }
+    Sexpr::List(Vec::new())
+}
